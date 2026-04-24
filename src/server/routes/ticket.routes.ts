bbc7cc0689fe1f02c6ee99bb3e69c0a85ba1fb7b
contrererick-ids <is722185@iteso.mx>
2026-04-24 16:37:51 -0600

    Add role-based access control middleware to routes

--- src/server/routes/ticket.routes.ts
+++ src/server/routes/ticket.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
+import { authenticateToken } from '../middlewares/auth.middleware';
+import { requireRole } from '../middlewares/role.middleware'
+import { UserRole} from '../models/user.model'
 import { createTicket, getTickets, getTicketByTicketId, reassignTicket, updateStatus, deleteTicket, addComment } from "../controllers/ticket.controller";
 
 const router = Router();
 
 // Rutas CRUD para tickets
-router.post("/new-ticket", createTicket);
-router.get("/get-all-tickets", getTickets);
-router.get("/get-ticket/:ticketId", getTicketByTicketId);
-router.delete("/delete-ticket/:ticketId", deleteTicket);
-router.patch("/reassign-ticket/:ticketId", reassignTicket);
-router.patch("/update-status/:ticketId", updateStatus);
-router.post("/add-comment/:ticketId", addComment);
+router.post("/new-ticket", authenticateToken, requireRole(UserRole.EXECUTIVE), createTicket);
+router.get("/get-all-tickets", authenticateToken, requireRole(UserRole.AGENT), getTickets);
+router.get("/get-ticket/:ticketId", authenticateToken, requireRole(UserRole.AGENT), getTicketByTicketId);
+router.delete("/delete-ticket/:ticketId", authenticateToken, requireRole(UserRole.ADMIN), deleteTicket);
+router.patch("/reassign-ticket/:ticketId", authenticateToken, requireRole(UserRole.AGENT), reassignTicket);
+router.patch("/update-status/:ticketId", authenticateToken, requireRole(UserRole.AGENT), updateStatus);
+router.post("/add-comment/:ticketId", authenticateToken, requireRole(UserRole.AGENT), addComment);
 
 export default router;
